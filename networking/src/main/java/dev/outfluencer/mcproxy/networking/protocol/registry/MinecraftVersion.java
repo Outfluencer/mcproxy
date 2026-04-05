b0a92d85be73e8a89702f402e89bec9d884ea63e
Outfluencer <git@outfluencer.dev>
2026-04-05 02:20:19 +0200

    Add supported versions list

--- networking/src/main/java/dev/outfluencer/mcproxy/networking/protocol/registry/MinecraftVersion.java
+++ networking/src/main/java/dev/outfluencer/mcproxy/networking/protocol/registry/MinecraftVersion.java
@@ -1,10 +1,12 @@
 package dev.outfluencer.mcproxy.networking.protocol.registry;
 
+import java.util.List;
+
 public final class MinecraftVersion {
 
     private MinecraftVersion() {}
 
     public static final int V1_21_11 = 774;
     public static final int V26_1 = 775;
-
+    public static final List<Integer> supportedVersions = List.of(V1_21_11, V26_1);
 }
